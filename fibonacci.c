ad2919548003b4fe0fccd7a80170f5eb56ff7a7f
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2022-12-06 21:12:07 +0530

    #include<stdio.h> int main() {     int i,n,t1,t2,t3;     scanf("%d",&n);     t1=0;     t2=1;     printf("%d %d ",t1,t2);     for(i=3;i<=n;i++)     {         t3=t1+t2;         printf("%d ",t3);         t1=t2;         t2=t3;     } }

---
+++ fibonacci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,t1,t2,t3;
+    scanf("%d",&n);
+    t1=0;
+    t2=1;
+    printf("%d %d ",t1,t2);
+    for(i=3;i<=n;i++)
+    {
+        t3=t1+t2;
+        printf("%d ",t3);
+        t1=t2;
+        t2=t3;
+    }
+}
