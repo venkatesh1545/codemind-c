1bcc3aa2f9414a72b3246749dc7a389d13c24af6
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2022-12-11 21:39:44 +0530

    #include<stdio.h> int main() {     int n,sum=0,r=0;     scanf("%d",&n);     while(n>0)     {     	r=n%10;     	if(r>sum)     	{     		sum=r; 		} 		n=n/10; 	}     printf("%d",sum); }

--- Largest_Digit.c
+++ Largest_Digit.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,a=0,r=0;
+    int n,sum=0,r=0;
     scanf("%d",&n);
     while(n>0)
     {
-        r=n%10;
-        if(r>a)
-        {
-            a=r;
-        }
-        n=n/10;
-    }
-    printf("%d",a);
+    	r=n%10;
+    	if(r>sum)
+    	{
+    		sum=r;
+		}
+		n=n/10;
+	}
+    printf("%d",sum);
 }
