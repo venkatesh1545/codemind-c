7ce8a23d6220c30be3e2251a7d5de95a2b75a4e1
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2022-12-11 21:37:42 +0530

    #include<stdio.h> int main() {     int n,a,b=1,temp;     scanf("%d",&n);     temp=n;     a=n*n;     while(n>0)     {     	b=b*10;     	n=n/10; 	}     if(a%b==temp)     {     	printf("Automorphic Number"); 	} 	else 	{ 		printf("Not an Automorphic Number"); 	} }

--- Automorphic_Number.c
+++ Automorphic_Number.c
@@ -7,15 +7,15 @@ int main()
     a=n*n;
     while(n>0)
     {
-        b=b*10;
-        n=n/10;
-    }
+    	b=b*10;
+    	n=n/10;
+	}
     if(a%b==temp)
     {
-        printf("Automorphic Number");
-    }
-    else
-    {
-        printf("Not an Automorphic Number");
-    }
+    	printf("Automorphic Number");
+	}
+	else
+	{
+		printf("Not an Automorphic Number");
+	}
 }
