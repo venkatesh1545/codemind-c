9644709fd0be9fae7886dcc06f74263b993a3c9f
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2022-12-28 21:38:19 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]<3)         {             printf("LIGHT ");         }         else if(a[i]>=3 && a[i]<7)         {             printf("MODERATE ");         }         else         {             printf("HEAVY ");         }     } }

---
+++ Rain_gauge.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<3)
+        {
+            printf("LIGHT
+");
+        }
+        else if(a[i]>=3 && a[i]<7)
+        {
+            printf("MODERATE
+");
+        }
+        else
+        {
+            printf("HEAVY
+");
+        }
+    }
+}
