afc9d543f5a117d557edaee0bcc655041880e117
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2022-12-28 23:31:14 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int a;         scanf("%d",&a);         if(a>30)         {             printf("YES ");         }         else         {             printf("NO ");         }     } }

---
+++ Subscription.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a;
+        scanf("%d",&a);
+        if(a>30)
+        {
+            printf("YES
+");
+        }
+        else
+        {
+            printf("NO
+");
+        }
+    }
+}
