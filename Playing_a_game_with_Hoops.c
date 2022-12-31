ae674950697f2005118c4684bd6f86cb04fb38ee
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2022-12-31 14:43:48 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     while(t--)     {         int a,b,c;         scanf("%d",&a);         b=a/2;         c=a-b;         printf("%d ",c);     } }

---
+++ Playing_a_game_with_Hoops.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int a,b,c;
+        scanf("%d",&a);
+        b=a/2;
+        c=a-b;
+        printf("%d
+",c);
+    }
+}
