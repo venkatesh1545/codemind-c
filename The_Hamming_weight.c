a6b065905ffe02eee8d6d2eaaf0ac75b7a6dfef5
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2023-01-04 18:52:58 +0530

    #include<stdio.h> int main() {     char str[100];     int i,c=0;     scanf("%s",&str);     for(i=0;str[i]!=NULL;i++)     {         if(str[i]=='1')         {             c=c+1;         }     }     printf("%d",c); }

---
+++ The_Hamming_weight.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,c=0;
+    scanf("%s",&str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]=='1')
+        {
+            c=c+1;
+        }
+    }
+    printf("%d",c);
+}
