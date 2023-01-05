3bf38a64af6b144de7d3ea625d42fd7c1f5a22f4
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2023-01-05 14:07:19 +0530

    #include<stdio.h> int main() { 	int n,i,j,p=1,sum=0; 	scanf("%d",&n); 	while(n>0) 	{ 		i=n%10; 		if(i%2==0) 		{ 			j=1; 		} 		else 		{ 			j=0; 		} 		sum=sum+j; 		p=p*j; 		n=n/10; 	} 	if(sum>0 && p==1) 	{ 		printf("Even"); 	} 	else if(sum==0 && p==0) 	{ 		printf("Odd"); 	} 	else 	{ 		printf("Mixed"); 	} }

--- Even_Odd_Mixed.c
+++ Even_Odd_Mixed.c
@@ -1,33 +1,33 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,sum=0,p=1,t;
-    scanf("%d",&n);
-    while(n>0)
-    {
-        i=n%10;
-        if(i%2==0)
-        {
-            t=1;
-        }
-        else
-        {
-            t=0;
-        }
-        sum=sum+t;
-        p=p*t;
-        n=n/10;
-    }
-    if(sum==0 && p==0)
-    {
-        printf("Odd");
-    }
-    else if(sum>0 && p==1)
-    {
-        printf("Even");
-    }
-    else
-    {
-        printf("Mixed");
-    }
+	int n,i,j,p=1,sum=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		i=n%10;
+		if(i%2==0)
+		{
+			j=1;
+		}
+		else
+		{
+			j=0;
+		}
+		sum=sum+j;
+		p=p*j;
+		n=n/10;
+	}
+	if(sum>0 && p==1)
+	{
+		printf("Even");
+	}
+	else if(sum==0 && p==0)
+	{
+		printf("Odd");
+	}
+	else
+	{
+		printf("Mixed");
+	}
 }
