909d4ec594c16a63c7447d9b2324d5b085c113e3
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2023-01-26 12:40:05 +0530

    #include<stdio.h> int main() {     int i,n,j,k,l,a[100],c=1,p=0,m=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(a[i]==a[j])             {                for(l=j;l<n;l++)                {                    a[l]=a[l+1];                }                j--;                n--;                c++;             }         }     if(c==k)     {     printf("%d ",a[i]);      p++;     }         c=1;     }    if(p==0)    {        printf("-1");    } }

---
+++ N_times_repeated_elements.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j,k,l,a[100],c=1,p=0,m=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+               for(l=j;l<n;l++)
+               {
+                   a[l]=a[l+1];
+               }
+               j--;
+               n--;
+               c++;
+            }
+        }    
+    if(c==k)
+    {
+    printf("%d ",a[i]);
+     p++;
+    }
+        c=1;
+    }
+   if(p==0)
+   {
+       printf("-1");
+   }
+}
