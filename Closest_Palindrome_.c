404dc7db74b27eace3327fb6f72bf1b41cdc37a8
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2023-05-20 22:32:13 +0530

    #include<stdio.h> int large(int a) { 	int b,i,s,r,lar; 	for(b=a+1;b>=0;b++) 	{ 	    i=b; 		s=0; 		while(i!=0) 		{ 		    r=i%10; 		    s=s*10+r; 		    i=i/10; 		} 		if(s==b) 		{ 			lar=s; 			break; 		} 	} 	return lar; } int small(int a) { 	int b,i,s,r,sma;     for(b=a-1;b>0;b--)     {         i=b; 	    s=0;         while(i!=0) 		{ 		    r=i%10; 		    s=s*10+r; 		    i=i/10; 		}         if(s==b) 	    { 		    sma=s; 		    break; 	    } 	} 	return sma; } int main() { 	int a,b,c,d,e,f,min; 	scanf("%d",&a); 	c=large(a); 	b=small(a);     e=c-a; 	f=a-b; 	if(e==f) 	{ 		printf("%d %d",b,c); 	}     else if(e<f) 	{ 		printf("%d",c); 	} 	else 	{ 	    printf("%d",b); 	} }

--- Closest_Palindrome_.c
+++ Closest_Palindrome_.c
@@ -1,59 +1,64 @@
 #include<stdio.h>
 int large(int a)
 {
-    int b,i,s,r,lar;
-    for(b=a+1;b>=0;b++)
-    {
-        i=b;
-        s=0;
-        while(i!=0)
-        {
-            r=i%10;
-            s=s*10+r;
-            i=i/10;
-        }
-        if(s==b)
-        {
-            lar=s;
-            // printf("%d",lar);
-            break;
-        }
-    }
-    return lar;
+	int b,i,s,r,lar;
+	for(b=a+1;b>=0;b++)
+	{
+	    i=b;
+		s=0;
+		while(i!=0)
+		{
+		    r=i%10;
+		    s=s*10+r;
+		    i=i/10;
+		}
+		if(s==b)
+		{
+			lar=s;
+			break;
+		}
+	}
+	return lar;
 }
 int small(int a)
 {
-    int i,s,r,sma,b;
+	int b,i,s,r,sma;
     for(b=a-1;b>0;b--)
     {
         i=b;
-        s=0;
+	    s=0;
         while(i!=0)
-        {
-            r=i%10;
-            s=s*10+r;
-            i=i/10;
-        }
+		{
+		    r=i%10;
+		    s=s*10+r;
+		    i=i/10;
+		}
         if(s==b)
-        {
-            sma=s;
-            break;
-        }
-    }
-    return sma;
+	    {
+		    sma=s;
+		    break;
+	    }	    
+	}
+	return sma;
 }
 int main()
 {
-    int a,b,c,d,e,f,min;
-    scanf("%d",&a);
-    b=small(a);
-    c=large(a);
+	int a,b,c,d,e,f,min;
+	scanf("%d",&a);
+	c=large(a);
+	b=small(a);
     e=c-a;
-    f=a-b;
-    if(e==f)
-    printf("%d %d",b,c);
+	f=a-b;
+	if(e==f)
+	{
+		printf("%d %d",b,c);
+	}
     else if(e<f)
-    printf("%d",c);
-    else
-    printf("%d",b);
+	{
+		printf("%d",c);
+	}
+	else
+	{
+	    printf("%d",b);
+	}
 }
