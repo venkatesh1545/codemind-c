5cfc74a6393669b2e1a9a156ff329bf376537445
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2023-05-20 22:51:29 +0530

    #include<stdio.h> int main() { 	int b,i,s,r,lar,a,k,l=0; 	scanf("%d",&a); 	for(b=a+1;b>0;b++) 	{ 		i=b; 		s=0; 		l=0; 		for(k=1;k<=b;k++) 		{ 			if(b%k==0) 			{ 				l++; 			} 		} 		if(l==2) 		{ 			while(i!=0) 			{ 				r=i%10; 				s=s*10+r; 				i=i/10; 			} 			if(s==b) 			{ 				lar=s; 				printf("%d",lar); 				break; 			} 		} 	} }

--- Program_to_find_out_the_next_prime_palindrome_number.c
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -1,52 +1,34 @@
 #include<stdio.h>
-#include<math.h>
-int is_prime(int n)
-{
-    int k,i;
-    k=pow(n,0.5);
-    for(i=2;i<k+1;i++)
-    {
-        if(n%i==0)
-        {
-            return 0;
-        }
-    }
-    return 1;
-}
-int is_palindrome(int n)
-{
-    int temp,rev,d;
-    temp=n;
-    rev=0;
-    while(n)
-    {
-        d=n%10;
-        n=n/10;
-        rev=(rev*10)+d;
-    }
-    n=temp;
-    if(n==rev)
-    {
-        return 1;
-    }
-    return 0;
-}
-
 int main()
 {
-    int n;
-    scanf("%d",&n);
-    while(true)
-    {
-        n+=1;
-        if(is_prime(n))
-      {
-        if(is_palindrome(n))
-        {
-            printf("%d",n);
-            break;
-        }
-      }
-    
-    }
+	int b,i,s,r,lar,a,k,l=0;
+	scanf("%d",&a);
+	for(b=a+1;b>0;b++)
+	{
+		i=b;
+		s=0;
+		l=0;
+		for(k=1;k<=b;k++)
+		{
+			if(b%k==0)
+			{
+				l++;
+			}
+		}
+		if(l==2)
+		{
+			while(i!=0)
+			{
+				r=i%10;
+				s=s*10+r;
+				i=i/10;
+			}
+			if(s==b)
+			{
+				lar=s;
+				printf("%d",lar);
+				break;
+			}
+		}
+	}
 }
