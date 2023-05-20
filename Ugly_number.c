db4fb7161693b76e3de15a212840e53549711d51
venkatesh1545 <119730058+venkatesh1545@users.noreply.github.com>
2023-05-20 21:57:18 +0530

    #include<stdio.h> int main() { 	int n,q,c=0; 	scanf("%d",&n); 	q=n; 	if(q>0) 	{ 		while(q!=0) 		{ 			if(q%2==0) 			{ 				q=q/2; 			} 			else if(q%3==0) 			{ 				q=q/3; 			} 			else if(q%5==0) 			{ 				q=q/5; 			} 			else 			{ 				c++; 				break; 			} 		} 		if(c==1) 		{ 			if(q==1) 			{ 				printf("Ugly Number"); 			} 			else 			{ 				printf("Not Ugly Number"); 			} 		} 	} 	else 	{ 	    printf("Not Ugly Number"); 	} }

--- Ugly_number.c
+++ Ugly_number.c
@@ -1,44 +1,45 @@
 #include<stdio.h>
 int main()
 {
-    int n,q,c=0;
-    scanf("%d",&n);
-    if(n>0)
-    {
-        while(n!=0)
-        {
-            if(n%2==0)
-            {
-                n=n/2;
-            }
-            else if(n%3==0)
-            {
-                n=n/3;
-            }
-            else if(n%5==0)
-            {
-                n=n/5;
-            }
-            else
-            {
-                c++;
-                break;
-            }
-        }
-        if(c==1)
-        {
-            if(n==1)
-            {
-                printf("Ugly Number");
-            }
-            else
-            {
-                printf("Not Ugly Number");
-            }
-        }
-    }
-    else
-    {
-        printf("Not Ugly Number");
-    }
+	int n,q,c=0;
+	scanf("%d",&n);
+	q=n;
+	if(q>0)
+	{
+		while(q!=0)
+		{
+			if(q%2==0)
+			{
+				q=q/2;
+			}
+			else if(q%3==0)
+			{
+				q=q/3;
+			}
+			else if(q%5==0)
+			{
+				q=q/5;
+			}
+			else
+			{
+				c++;
+				break;
+			}
+		}
+		if(c==1)
+		{
+			if(q==1)
+			{
+				printf("Ugly Number");
+			}
+			else
+			{
+				printf("Not Ugly Number");
+			}
+		}
+	}
+	else
+	{
+	    printf("Not Ugly Number");
+	}
 }
